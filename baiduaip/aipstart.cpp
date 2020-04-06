6ddb9d61047305c6c6dccd20148cc0ffbc49a66c
y <1589082159@qq.com>
2020-04-06 10:19:22 +0800

    1

---
+++ baiduaip/aipstart.cpp
@@ -0,0 +1,9 @@
+#inlucde"imageclassify.h"
+
+//APPID/AK/SK
+
+std::string app_id= "19237731";
+std::string api_key= "EbKtLMi1icxtlYbrO2fzKuQn";
+std::string secret_key= "ZEwzbcVllFqz5PAsFzKkl3ttYsGxneZp";
+
+aip::Imageclassify client(app_id,api_key,secret_key);
---
+++ client.h
@@ -0,0 +1,5 @@
+#include "baiduaip/imageclassify.h"
+
+//APPID/AK/SK
+
+std::string app_id = ""
