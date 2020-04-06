3c74a0ae5c5167df8a87c4d4b44bd25aaf84f92b
yejunhui <1589082159@qq.com>
2020-04-06 10:45:18 +0800

    1

---
+++ client.cpp
@@ -0,0 +1,9 @@
+#inlucde<iostream>
+#inlcude"client.h"
+
+using namespect std;
+
+int main(){
+	carDetection c;
+	string a = c.carDetection("/c/Users/user/Pictures/微信图片_20200318112922.jpg");
+}
--- client.h
+++ client.h
@@ -2,4 +2,24 @@
 
 //APPID/AK/SK
 
-std::string app_id = ""
+std::string app_id = "19237731";
+std::string api_key = "EbKtLMi1icxtlYbrO2fzKuQn";
+std::string secret_key = "ZEwzbcVlIFQz5PAsFzKkI3ttYsGXneZp";
+
+aip::Imageclassify client(app_id,api_key,secret_key);
+
+Json::Value result;
+std::string;
+
+//车辆检测
+class carDetection(){
+
+	//aip::get_file_content()
+
+	carDetection(std::string fileName){
+		aip::get_file_content(fileName,&image);
+		result = client.vehicle_detect(image,aip::null);
+		return result;
+	}
+
+}
